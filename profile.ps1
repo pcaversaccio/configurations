f326d56310b5165da728c3cf57bd002174ee3c5c
Pascal Marco Caversaccio <pcaversaccio@users.noreply.github.com>
2023-04-27 10:58:10 +0200

    😎 Add venv aliases

--- profile.ps1
+++ profile.ps1
@@ -1,1 +1,20 @@
+# Set `vim` alias
 Set-alias vi "C:\Program Files (x86)\Vim\vim90\vim.exe"
+
+# Set alias to create and activate a virtualenv on Windows using the `python` command
+Set-alias venv createPythonEnvironment
+
+# Set alias to create and activate a virtualenv on Windows using the `python3` command
+Set-alias venv3 createPython3Environment
+
+# Helper function for the `venv` alias using the `python` command
+function createPythonEnvironment {
+    python -m venv venv # Create the virtualenv
+    venv\Scripts\activate # Activate the virtualenv
+}
+
+# Helper function for the `venv3` alias using the `python3` command
+function createPython3Environment {
+    python3 -m venv venv # Create the virtualenv
+    venv\Scripts\activate # Activate the virtualenv
+}
